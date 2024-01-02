51c16a98b717e52d665dc645d5f28debc5f234e7
HYEONJI LEE <8976guswl@naver.com>
2024-01-02 10:32:03 +0900

    [UPDATE] 재화 데이터 컨트롤러 기능 추가

--- Assets/Scripts/DataControllers/CurrencyDataController.cs
+++ Assets/Scripts/DataControllers/CurrencyDataController.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrencyDataController : MonoBehaviour
+public class CurrencyDataController : DataController
 {
-    // Start is called before the first frame update
-    void Start()
+    // 모든 통화를 로컬에 저장시키는 메서드
+    public void SaveCurrencies(List<Currency> currencies)
     {
-        
+        ES3.Save<List<Currency>>("currencies", currencies);
     }
 
-    // Update is called once per frame
-    void Update()
+    // 로컬에 저장되어있는 모든 통화를 불러오는 메서드
+    public bool LoadCurrencies(List<Currency> currencies, Action<string, string> OnCurrencyChanged)
     {
-        
+        if (ES3.KeyExists("currencies"))
+        {
+            currencies = ES3.Load<List<Currency>>("currencies");
+            foreach (Currency currency in currencies)
+            {
+                OnCurrencyChanged?.Invoke(currency.currencyName, currency.amount); // 로딩 후 이벤트 발생
+            }
+        }
+        else return false;
+        return true;
     }
 }
